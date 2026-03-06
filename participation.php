b0c424493cec0b3fe58446d91f3c82bc78bbd732
Rémi Sagnes <rems.sagnes@gmail.com>
2026-03-06 19:39:32 +0100

    préparation des endpoints pour la suite et suppression de ceux servant à rien

--- participation.php
+++ participation.php
@@ -3,6 +3,6 @@
 require_once __DIR__ . '/autoload.php';
 require_once __DIR__ . '/utils.php';
 
-echo "feuilledematch";
+echo "participation";
 
 ?>
--- utilisateur_PASSURDECE_ENDPOINT.php
+++ utilisateur_PASSURDECE_ENDPOINT.php
@@ -3,6 +3,6 @@
 require_once __DIR__ . '/autoload.php';
 require_once __DIR__ . '/utils.php';
 
-echo "match";
+echo "utilisateur";
 
 ?>
